c7f4ba880dbd7d0ff806a25c5fdf167e4ee59d2d
Djaymare <djayp34@gmail.com>
2026-01-02 17:09:13 +0100

    fix svgs

--- stage-project/src/components/DarkMode.vue
+++ stage-project/src/components/DarkMode.vue
@@ -1,34 +1,40 @@
 <script setup lang="ts">
 import useDarkMode from '../composable/useDarkMode';
-import Light from '../assets/light-mode.svg';
-import Dark from '../assets/light-mode.svg';
+import Light from '../assets/sun-line.svg';
+import Dark from '../assets/moon-line.svg';
 
 const { isDark, setDarkMode } = useDarkMode();
 </script>
 
 <template>
     <button @click="setDarkMode">
-        <Light
-            v-if="isDark"
-            alt="Light mode"
-            width="20"
-            height="20"
-        />
-        <Dark
-            v-else
-            alt="Dark mode"
-            width="20"
-            height="20"
-        />
+        <div id="icon-container">
+            <Light
+                v-if="isDark"
+                alt="Light mode"
+            />
+            <Dark
+                v-else
+                alt="Dark mode"
+            />   
+        </div>
     </button>
 </template>
 
 <style scoped>
 button {
+    width: 32px;
     height: 32px;
+    margin: 0;
+    padding: 4px 4px;
 }
-svg {
-    max-width: 24px;
-    height: auto;
+#icon-container {
+    width: 22.5px;
+    height: 22.5px;
+}
+
+#icon-container svg {
+    width: 100%;
+    height: 100%;
 }
 </style>
